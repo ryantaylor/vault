b6eadda683922ce441b93ca3afc8df55d1b69682
ryantaylor <ryan.taylor9@gmail.com>
2015-10-30 22:18:02 -0400

    Updated script to check version properly (I think).

--- build-doc.sh
+++ build-doc.sh
@@ -1,4 +1,4 @@
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ] && [ "$TRAVIS_RUST_VERSION" = '1.4.0' ]; then
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ] && [ "$TRAVIS_RUST_VERSION" = 'rustc 1.4.0 (8ab8581f6 2015-10-27)' ]; then
   # Fetch the docs
   git clone https://github.com/ryantaylor/vault.git docs --branch gh-pages
   
@@ -15,3 +15,5 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ] && [ 
   git commit -m "(docs-autogen) ${TRAVIS_REPO_SLUG}."
   git push -q "https://${TOKEN}:x-oauth-basic@github.com/ryantaylor/vault.git" gh-pages
 fi
+
+echo 'doc upload completed'
