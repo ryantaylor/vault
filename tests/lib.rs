e3a29a54c2397a77842a4309c8cc188d2b0faddc
ryantaylor <ryan.taylor9@gmail.com>
2015-12-04 12:21:23 -0500

    Fixed tests.

--- tests/lib.rs
+++ tests/lib.rs
@@ -4,14 +4,10 @@ extern crate vault;
 
 use std::path::Path;
 
-use vault::Vault;
-
 #[test]
 fn parse() {
     let path_str = format!("{}/replays/bench.rec", env!("CARGO_MANIFEST_DIR"));
     let path = Path::new(&path_str);
-    let vault = Vault::parse(&path).unwrap();
-    for replay in &vault.replays {
-        assert_eq!(replay.error, None);
-    }
+    let replay = vault::parse_replay(&path, None).unwrap();
+    assert_eq!(replay.error, None);
 }
