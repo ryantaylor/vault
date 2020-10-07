b9b8856c18c5ea7fc65bcdad46743da3166a8593
ryantaylor <ryan.taylor9@gmail.com>
2020-10-07 08:43:35 -0400

    Rewrite parsing for nom 5.

--- src/parser.rs
+++ src/parser.rs
@@ -1,179 +1,242 @@
 use std::fmt::Debug;
+use std::io::Cursor;
 use std::ops::{RangeTo, RangeFrom, Deref};
 use std::slice::SliceIndex;
 use std::str;
+use std::string::String;
 
-use nom::{le_u16, IResult, Needed, need_more, InputTake, InputLength, AtEof, AsBytes, Slice};
-use nom::types::CompleteByteSlice;
+use byteorder::{LittleEndian, ReadBytesExt};
 
-named!(zero_u16<u16>, verify!(le_u16, |n: u16| n == 0));
+// use nom::{le_u16, IResult, Needed, need_more, InputTake, InputLength, AtEof, AsBytes, Slice};
+// use nom::types::CompleteByteSlice;
+use nom::{IResult};
+use nom::bytes::complete::{take, take_till, take_while};
+// use nom::error::ParseError;
+use nom::combinator::{map_res, verify};
+use nom::number::complete::{le_u16, le_u32};
+use nom::sequence::{preceded};
 
-named!(pub match_version<u16>,
-    do_parse!(
-        zero_u16 >>
-        version: le_u16 >>
-        (version)
-    )
-);
-
-pub fn match_utf8<'a>(i: &'a [u8], value: &str) -> IResult<&'a [u8], &'a str> {
-    map_res!(i, tag!(value), str::from_utf8)
+fn verify_zero_u16(input: &[u8]) -> IResult<&[u8], u16> {
+    verify(le_u16, |n: &u16| *n == 0)(input)
 }
 
-named!(pub match_terminated_utf16<String>,
-    map!(
-        many_till!(le_u16, zero_u16),
-        |(result, _)| String::from_utf16_lossy(&result)
-    )
-);
+// fn verify_zero_u16<'a, O2, E: ParseError<&'a [u8]>, F, G>(
+//     first: F,
+//     second: G
+// ) -> impl Fn(&'a [u8]) -> IResult<&'a [u8], u16, E> where
+//     F: Fn(&'a [u8]) -> IResult<&'a [u8], u16, E>,
+//     G: Fn(&O2) -> bool,
+//     O2: ?Sized {
+//         verify(le_u16, |n: &u16| *n == 0)
+//     }
+// fn verify_zero_u16<'a, E: ParseError<&'a [u8]>>() -> impl Fn(&'a [u8]) -> IResult<&'a [u8], u16, E> {
+//     verify(le_u16, |n: &u16| *n == 0)
+// }
+// named!(zero_u16<u16>, verify!(le_u16, |n: u16| n == 0));
 
-pub fn cbs_le_u16(i: CompleteByteSlice) -> IResult<CompleteByteSlice, u16> {
-  if i.len() < 2 {
-    need_more(i, Needed::Size(2))
-  } else {
-    let res = ((i[1] as u16) << 8) + i[0] as u16;
-    Ok((CompleteByteSlice(&i[2..]), res))
-  }
+pub fn parse_version(input: &[u8]) -> IResult<&[u8], u16> {
+    // let zero = verify(le_u16, |n: &u16| *n == 0);
+    preceded(verify_zero_u16, le_u16)(input)
+}
+// named!(pub match_version<u16>,
+//     do_parse!(
+//         zero_u16 >>
+//         version: le_u16 >>
+//         (version)
+//     )
+// );
+
+pub fn parse_utf8<'a>(len: u8) -> impl Fn(&'a [u8]) -> IResult<&'a [u8], &str> {
+    map_res(take(len), |s: &[u8]| str::from_utf8(s))
 }
 
-pub fn orig_le_u16(i: &[u8]) -> IResult<&[u8], u16> {
-  if i.len() < 2 {
-    need_more(i, Needed::Size(2))
-  } else {
-    let res = ((i[1] as u16) << 8) + i[0] as u16;
-    Ok((&i[2..], res))
-  }
+pub fn parse_game_type(input: &[u8]) -> IResult<&[u8], &str> {
+    parse_utf8(8)(input)
 }
 
-// pub fn b_le_u16<T>(i: T) -> IResult<T, u16> where T: InputLength + Deref + AtEof {
-//     if i.input_len() < 2 {
-//         need_more(i, Needed::Size(2))
+pub fn parse_utf16(input: &[u8]) -> IResult<&[u8], String> {
+    let (input, u8_slice) = take_till(|c| c == 0)(input)?;
+
+    let mut u16_vec = Vec::with_capacity(u8_slice.len() / 2);
+    let mut cursor = Cursor::new(u8_slice);
+
+    cursor.read_u16_into::<LittleEndian>(&mut u16_vec).unwrap();
+
+    let result = String::from_utf16(&u16_vec).unwrap();
+
+    Ok((input, result))
+}
+
+pub fn take_zeroes(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    take_while(|n: u8| n == 0)(input)
+}
+
+pub fn verify_le_u32<'a>(expected: u32) -> impl Fn(&'a [u8]) -> IResult<&'a [u8], u32> {
+    verify(le_u32, move |n: &u32| *n == expected)
+}
+
+// fn parse_utf16(input: &[u8]) -> IResult<&[u8], &str> {
+//     map_res(take_till(|c| c == 0), |s: &[u8]| String::from_utf16(s as &[u16])?.as_str)(input)
+// }
+
+// pub fn match_utf8<'a>(i: &'a [u8], value: &str) -> IResult<&'a [u8], &'a str> {
+//     map_res!(i, tag!(value), str::from_utf8)
+// }
+
+// named!(pub match_terminated_utf16<String>,
+//     map!(
+//         many_till!(le_u16, zero_u16),
+//         |(result, _)| String::from_utf16_lossy(&result)
+//     )
+// );
+
+// pub fn cbs_le_u16(i: CompleteByteSlice) -> IResult<CompleteByteSlice, u16> {
+//   if i.len() < 2 {
+//     need_more(i, Needed::Size(2))
+//   } else {
+//     let res = ((i[1] as u16) << 8) + i[0] as u16;
+//     Ok((CompleteByteSlice(&i[2..]), res))
+//   }
+// }
+
+// pub fn orig_le_u16(i: &[u8]) -> IResult<&[u8], u16> {
+//   if i.len() < 2 {
+//     need_more(i, Needed::Size(2))
+//   } else {
+//     let res = ((i[1] as u16) << 8) + i[0] as u16;
+//     Ok((&i[2..], res))
+//   }
+// }
+
+// // pub fn b_le_u16<T>(i: T) -> IResult<T, u16> where T: InputLength + Deref + AtEof {
+// //     if i.input_len() < 2 {
+// //         need_more(i, Needed::Size(2))
+// //     } else {
+// //         let res = ((i[1] as u16) << 8) + i[0] as u16;
+// //         Ok((&i[2..], res))
+// //     }
+// // }
+
+// pub fn g_le_u8<T>(i: T) -> IResult<T, u8>
+// where T: InputLength + AtEof + AsBytes + Slice<RangeTo<usize>> + Slice<RangeFrom<usize>> {
+//     if i.input_len() < 1 {
+//         need_more(i, Needed::Size(1))
 //     } else {
-//         let res = ((i[1] as u16) << 8) + i[0] as u16;
-//         Ok((&i[2..], res))
+//         let buf = i.slice(..1);
+//         let bytes = buf.as_bytes();
+
+//         Ok((i.slice(1..), bytes[0]))
 //     }
 // }
 
-pub fn g_le_u8<T>(i: T) -> IResult<T, u8>
-where T: InputLength + AtEof + AsBytes + Slice<RangeTo<usize>> + Slice<RangeFrom<usize>> {
-    if i.input_len() < 1 {
-        need_more(i, Needed::Size(1))
-    } else {
-        let buf = i.slice(..1);
-        let bytes = buf.as_bytes();
+// pub fn g_le_u16<T>(i: T) -> IResult<T, u16>
+// where T: InputLength + AtEof + AsBytes + Slice<RangeTo<usize>> + Slice<RangeFrom<usize>> {
+//     if i.input_len() < 2 {
+//         need_more(i, Needed::Size(2))
+//     } else {
+//         let buf = i.slice(..2);
+//         let bytes = buf.as_bytes();
 
-        Ok((i.slice(1..), bytes[0]))
-    }
-}
+//         let res = ((bytes[1] as u16) << 8) + bytes[0] as u16;
+//         Ok((i.slice(2..), res))
+//     }
+// }
 
-pub fn g_le_u16<T>(i: T) -> IResult<T, u16>
-where T: InputLength + AtEof + AsBytes + Slice<RangeTo<usize>> + Slice<RangeFrom<usize>> {
-    if i.input_len() < 2 {
-        need_more(i, Needed::Size(2))
-    } else {
-        let buf = i.slice(..2);
-        let bytes = buf.as_bytes();
+// pub fn g_le_u32<T>(i: T) -> IResult<T, u32>
+// where T: InputLength + AtEof + AsBytes + Slice<RangeTo<usize>> + Slice<RangeFrom<usize>> {
+//     if i.input_len() < 4 {
+//         need_more(i, Needed::Size(4))
+//     } else {
+//         let buf = i.slice(..4);
+//         let bytes = buf.as_bytes();
 
-        let res = ((bytes[1] as u16) << 8) + bytes[0] as u16;
-        Ok((i.slice(2..), res))
-    }
-}
+//         let res = ((bytes[3] as u32) << 24) + ((bytes[2] as u32) << 16) + ((bytes[1] as u32) << 8) + bytes[0] as u32;
+//         Ok((i.slice(4..), res))
+//     }
+// }
 
-pub fn g_le_u32<T>(i: T) -> IResult<T, u32>
-where T: InputLength + AtEof + AsBytes + Slice<RangeTo<usize>> + Slice<RangeFrom<usize>> {
-    if i.input_len() < 4 {
-        need_more(i, Needed::Size(4))
-    } else {
-        let buf = i.slice(..4);
-        let bytes = buf.as_bytes();
+// pub fn g_le_u64<T>(i: T) -> IResult<T, u64>
+// where T: InputLength + AtEof + AsBytes + Slice<RangeTo<usize>> + Slice<RangeFrom<usize>> {
+//     if i.input_len() < 8 {
+//         need_more(i, Needed::Size(8))
+//     } else {
+//         let buf = i.slice(..8);
+//         let bytes = buf.as_bytes();
 
-        let res = ((bytes[3] as u32) << 24) + ((bytes[2] as u32) << 16) + ((bytes[1] as u32) << 8) + bytes[0] as u32;
-        Ok((i.slice(4..), res))
-    }
-}
+//         let res = ((bytes[7] as u64) << 56) + ((bytes[6] as u64) << 48) + ((bytes[5] as u64) << 40) + ((bytes[4] as u64) << 32) +
+//             ((bytes[3] as u64) << 24) + ((bytes[2] as u64) << 16) + ((bytes[1] as u64) << 8) + bytes[0] as u64;
+//         Ok((i.slice(8..), res))
+//     }
+// }
 
-pub fn g_le_u64<T>(i: T) -> IResult<T, u64>
-where T: InputLength + AtEof + AsBytes + Slice<RangeTo<usize>> + Slice<RangeFrom<usize>> {
-    if i.input_len() < 8 {
-        need_more(i, Needed::Size(8))
-    } else {
-        let buf = i.slice(..8);
-        let bytes = buf.as_bytes();
+// #[cfg(test)]
+// mod tests {
+//     use byteorder::{LittleEndian, WriteBytesExt};
 
-        let res = ((bytes[7] as u64) << 56) + ((bytes[6] as u64) << 48) + ((bytes[5] as u64) << 40) + ((bytes[4] as u64) << 32) +
-            ((bytes[3] as u64) << 24) + ((bytes[2] as u64) << 16) + ((bytes[1] as u64) << 8) + bytes[0] as u64;
-        Ok((i.slice(8..), res))
-    }
-}
+//     use super::*;
 
-#[cfg(test)]
-mod tests {
-    use byteorder::{LittleEndian, WriteBytesExt};
+//     #[test]
+//     fn test_parse_match_version_ok() {
+//         let input = [0, 0, 1, 2];
+//         let (_, val) = match_version(&input).unwrap();
+//         assert_eq!(val, 513);
+//     }
 
-    use super::*;
+//     #[test]
+//     fn test_parse_match_version_err_first() {
+//         let input = [1, 0, 1, 2];
+//         let error = match match_version(&input) {
+//             Err(_) => true,
+//             _ => false
+//         };
+//         assert!(error);
+//     }
 
-    #[test]
-    fn test_parse_match_version_ok() {
-        let input = [0, 0, 1, 2];
-        let (_, val) = match_version(&input).unwrap();
-        assert_eq!(val, 513);
-    }
+//     #[test]
+//     fn test_parse_match_version_err_second() {
+//         let input = [0, 1, 1, 2];
+//         let error = match match_version(&input) {
+//             Err(_) => true,
+//             _ => false
+//         };
+//         assert!(error);
+//     }
 
-    #[test]
-    fn test_parse_match_version_err_first() {
-        let input = [1, 0, 1, 2];
-        let error = match match_version(&input) {
-            Err(_) => true,
-            _ => false
-        };
-        assert!(error);
-    }
+//     #[test]
+//     fn test_match_utf8_ok() {
+//         let input = b"COH2_REC";
+//         let (_, val) = match_utf8(input, "COH2_REC").unwrap();
+//         assert_eq!(val, "COH2_REC");
+//     }
 
-    #[test]
-    fn test_parse_match_version_err_second() {
-        let input = [0, 1, 1, 2];
-        let error = match match_version(&input) {
-            Err(_) => true,
-            _ => false
-        };
-        assert!(error);
-    }
+//     #[test]
+//     fn test_match_utf8_err() {
+//         let input = b"COH_REC";
+//         let error = match match_utf8(input, "COH2_REC") {
+//             Err(_) => true,
+//             _ => false
+//         };
+//         assert!(error);
+//     }
 
-    #[test]
-    fn test_match_utf8_ok() {
-        let input = b"COH2_REC";
-        let (_, val) = match_utf8(input, "COH2_REC").unwrap();
-        assert_eq!(val, "COH2_REC");
-    }
+//     #[test]
+//     fn test_match_terminated_utf16() {
+//         let input = to_utf16_slice("11/7/2015");
 
-    #[test]
-    fn test_match_utf8_err() {
-        let input = b"COH_REC";
-        let error = match match_utf8(input, "COH2_REC") {
-            Err(_) => true,
-            _ => false
-        };
-        assert!(error);
-    }
+//         let (_, val) = match_terminated_utf16(&input).unwrap();
+//         assert_eq!(val, "11/7/2015");
+//     }
 
-    #[test]
-    fn test_match_terminated_utf16() {
-        let input = to_utf16_slice("11/7/2015");
+//     fn to_utf16_slice(input: &str) -> Vec<u8> {
+//         let mut as_vec: Vec<u16> = input.encode_utf16().collect();
+//         as_vec.push(0);
+//         let slice_u16: &[u16] = &as_vec;
 
-        let (_, val) = match_terminated_utf16(&input).unwrap();
-        assert_eq!(val, "11/7/2015");
-    }
+//         let mut u8s: Vec<u8> = Vec::new();
+//         for &n in slice_u16 {
+//             let _ = u8s.write_u16::<LittleEndian>(n);
+//         }
 
-    fn to_utf16_slice(input: &str) -> Vec<u8> {
-        let mut as_vec: Vec<u16> = input.encode_utf16().collect();
-        as_vec.push(0);
-        let slice_u16: &[u16] = &as_vec;
-
-        let mut u8s: Vec<u8> = Vec::new();
-        for &n in slice_u16 {
-            let _ = u8s.write_u16::<LittleEndian>(n);
-        }
-
-        u8s
-    }
-}
+//         u8s
+//     }
+// }
--- src/replay_service.rs
+++ src/replay_service.rs
@@ -1,13 +1,24 @@
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-use test::Bencher;
+use std::option::Option;
+// use test::Bencher;
 
-use nom::{le_u8, le_u16, le_u32, IResult};
-use nom::types::CompleteByteSlice;
+// use nom::{le_u8, le_u16, le_u32, IResult};
+// use nom::types::CompleteByteSlice;
+
+use nom::IResult;
+use nom::branch::{alt};
+use nom::bytes::complete::{tag};
+use nom::combinator::{map, peek};
+use nom::multi::{many0};
+use nom::number::complete::{le_u32};
+use nom::sequence::{preceded, tuple};
 
 use new_replay::NewReplay;
-use parser::{orig_le_u16, g_le_u8, g_le_u16, g_le_u32, g_le_u64, cbs_le_u16, match_utf8, match_version, match_terminated_utf16};
+// use parser::{orig_le_u16, g_le_u8, g_le_u16, g_le_u32, g_le_u64, cbs_le_u16, match_utf8, match_version, match_terminated_utf16};
+
+use parser::{parse_utf8, parse_version, parse_game_type, parse_utf16, take_zeroes, verify_le_u32};
 
 const GAME_TYPE: &'static str = "COH2_REC";
 const CHUNKY_NAME: &'static str = "Relic Chunky";
@@ -20,102 +31,305 @@ pub fn parse(path: &Path) -> bool {
     let mut buff: Vec<u8> = Vec::new();
     file.read_to_end(&mut buff).unwrap();
 
-    let (remaining, replay) = parse_header(&buff).unwrap();
+    let (remaining, replay) = parse_replay(&buff).unwrap();
     true
 }
 
-named!(parse_header<(u16, &str, String)>,
-    do_parse!(
-        version: match_version >>
-        game_type: apply!(match_utf8, GAME_TYPE) >>
-        timestamp: match_terminated_utf16 >>
-        many_m_n!(7, 7, verify!(le_u32, |n: u32| n == 0)) >>
-        (version, game_type, timestamp)
-    )
-);
-
-named!(parse_chunky<bool>,
-    do_parse!(
-        apply!(match_utf8, CHUNKY_NAME) >>
-        verify!(le_u32, |n| n == CHUNKY_TYPE) >>
-        verify!(le_u32, |n| n == CHUNKY_VERSION) >>
-        verify!(le_u32, |n| n == 0x1) >>
-        verify!(le_u32, |n| n == 0x24) >>
-        verify!(le_u32, |n| n == 0x1C) >>
-        verify!(le_u32, |n| n == 0x1) >>
-        (true)
-    )
-);
-
-named!(test_eof<CompleteByteSlice, bool>,
-    do_parse!(
-        // many_till!(g_le_u16, eof!()) >>
-        count!(g_le_u8, 2000000) >>
-        (true)
-    )
-);
-
-named!(test_eof_slice<bool>,
-    do_parse!(
-        count!(g_le_u8, 2000000) >>
-        (true)
-    )
-);
-
-// fn test_eof_long(input: CompleteByteSlice) -> IResult<CompleteByteSlice, CompleteByteSlice> {
-//     many_till!(input, take!(1), eof!())
-// }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_parse_header_ok() {
-        let buff = read_into_buffer(Path::new("/Users/ryantaylor/Code/vault/replays/bench.rec"));
-        let (_, (version, game_type, timestamp)) = parse_header(&buff).unwrap();
-        assert_eq!(version, 20297);
-        assert_eq!(game_type, "COH2_REC");
-        assert_eq!(timestamp, "11/7/2015 1:16 AM");
-    }
-
-    #[test]
-    fn test_parse_chunky_ok() {
-        let buff = read_into_buffer(Path::new("/Users/ryantaylor/Code/vault/replays/bench.rec"));
-        let (remaining, _) = parse_header(&buff).unwrap();
-        let (_, result) = parse_chunky(remaining).unwrap();
-        assert!(result);
-    }
-
-    #[test]
-    fn test_parse_eof() {
-        let buff = read_into_buffer(Path::new("/Users/ryantaylor/Code/vault/replays/bench.rec"));
-        let (remaining, _) = test_eof(CompleteByteSlice(&buff)).unwrap();
-    }
-
-    #[bench]
-    fn bench_parse_eof(b: &mut Bencher) {
-        let buff = read_into_buffer(Path::new("/Users/ryantaylor/Code/vault/replays/bench.rec"));
-        b.iter(|| {
-            test_eof(CompleteByteSlice(&buff)).unwrap()
-            // println!("{:?}", remaining);
-            // println!("{:?}", val);
-        });
-    }
-
-    #[bench]
-    fn bench_parse_eof_slice(b: &mut Bencher) {
-        let buff = read_into_buffer(Path::new("/Users/ryantaylor/Code/vault/replays/bench.rec"));
-        b.iter(|| {
-            test_eof_slice(&buff).unwrap()
-            // println!("{:?}", remaining);
-        });
-    }
-
-    fn read_into_buffer(path: &Path) -> Vec<u8> {
-        let mut file = File::open(path).unwrap();
-        let mut buff: Vec<u8> = Vec::new();
-        file.read_to_end(&mut buff).unwrap();
-        buff
-    }
+fn parse_replay(input: &[u8]) -> IResult<&[u8], NewReplay> {
+    let (input, (version, game_type, timestamp)) = tuple((parse_version, parse_game_type, parse_utf16))(input)?;
+    Ok((input, NewReplay::new(version, game_type, timestamp)))
 }
+
+struct Header {
+    pub version: u32,
+    pub game_type: String,
+    pub timestamp: String
+}
+
+fn parse_header(input: &[u8]) -> IResult<&[u8], Header> {
+    map(
+        tuple((
+            le_u32,
+            parse_utf8(8),
+            parse_utf16,
+            take_zeroes
+        )),
+        |(
+            version,
+            game_type,
+            timestamp,
+            _
+        )| {
+            Header {
+                version,
+                timestamp,
+                game_type: game_type.to_owned()
+            }
+        }
+    )(input)
+}
+
+struct RelicChunky {
+    pub name: String,
+    pub signature: u32,
+    pub major_version: u32,
+    pub minor_version: u32, // maybe?
+    pub chunk_offset: u32, // bytes from start of chunky to start of first member chunk
+    pub unknown_offset: u32, // usually 0x1C
+    pub unknown_id: u32, // usually 0x1
+    pub chunks: Vec<RelicChunk>
+}
+
+fn parse_chunky(input: &[u8]) -> IResult<&[u8], RelicChunky> {
+    map(
+        tuple((
+            tag("Relic Chunky"),
+            verify_le_u32(0x1A0A0D),
+            verify_le_u32(0x3),
+            verify_le_u32(0x3),
+            le_u32,
+            le_u32,
+            le_u32,
+            many0(parse_chunk)
+        )),
+        |(
+            name,
+            signature,
+            major_version,
+            minor_version,
+            chunk_offset,
+            unknown_offset,
+            unknown_id,
+            chunks
+        )| {
+            RelicChunky {
+                name: String::from_utf8_lossy(name).into_owned(),
+                signature,
+                major_version,
+                minor_version,
+                chunk_offset,
+                unknown_offset,
+                unknown_id,
+                chunks
+            }
+        }
+    )(input)
+}
+
+struct RelicChunk {
+    pub chunk_kind: String,
+    pub chunk_type: String
+}
+
+fn parse_chunk(input: &[u8]) -> IResult<&[u8], RelicChunk> {
+    alt((
+        parse_data_chunk, parse_folder_chunk
+    ))(input)
+}
+
+fn parse_folder_chunk(input: &[u8]) -> IResult<&[u8], RelicChunk> {
+    preceded(
+        peek(tag("FOLD")),
+        alt((
+            parse_foldinfo_chunk,
+            parse_foldpost_chunk
+        ))
+    )(input)
+}
+
+fn parse_foldinfo_chunk(input: &[u8]) -> IResult<&[u8], RelicChunk> {
+    map(
+        tuple((
+            tag("FOLD"),
+            tag("INFO")
+        )),
+        |(
+            chunk_kind,
+            chunk_type
+        )| {
+            RelicChunk {
+                chunk_kind: String::from_utf8_lossy(chunk_kind).into_owned(),
+                chunk_type: String::from_utf8_lossy(chunk_type).into_owned()
+            }
+        }
+    )(input)
+}
+
+fn parse_foldpost_chunk(input: &[u8]) -> IResult<&[u8], RelicChunk> {
+    map(
+        tuple((
+            tag("FOLD"),
+            tag("POST")
+        )),
+        |(
+            chunk_kind,
+            chunk_type
+        )| {
+            RelicChunk {
+                chunk_kind: String::from_utf8_lossy(chunk_kind).into_owned(),
+                chunk_type: String::from_utf8_lossy(chunk_type).into_owned()
+            }
+        }
+    )(input)
+}
+
+fn parse_data_chunk(input: &[u8]) -> IResult<&[u8], RelicChunk> {
+    preceded(
+        peek(tag("DATA")),
+        alt((
+            parse_datasdsc_chunk,
+            parse_datadata_chunk,
+            parse_dataplas_chunk
+        ))
+    )(input)
+}
+
+fn parse_datasdsc_chunk(input: &[u8]) -> IResult<&[u8], RelicChunk> {
+    map(
+        tuple((
+            tag("DATA"),
+            tag("SDSC")
+        )),
+        |(
+            chunk_kind,
+            chunk_type
+        )| {
+            RelicChunk {
+                chunk_kind: String::from_utf8_lossy(chunk_kind).into_owned(),
+                chunk_type: String::from_utf8_lossy(chunk_type).into_owned()
+            }
+        }
+    )(input)
+}
+
+fn parse_datadata_chunk(input: &[u8]) -> IResult<&[u8], RelicChunk> {
+    map(
+        tuple((
+            tag("DATA"),
+            tag("DATA")
+        )),
+        |(
+            chunk_kind,
+            chunk_type
+        )| {
+            RelicChunk {
+                chunk_kind: String::from_utf8_lossy(chunk_kind).into_owned(),
+                chunk_type: String::from_utf8_lossy(chunk_type).into_owned()
+            }
+        }
+    )(input)
+}
+
+fn parse_dataplas_chunk(input: &[u8]) -> IResult<&[u8], RelicChunk> {
+    map(
+        tuple((
+            tag("DATA"),
+            tag("PLAS")
+        )),
+        |(
+            chunk_kind,
+            chunk_type
+        )| {
+            RelicChunk {
+                chunk_kind: String::from_utf8_lossy(chunk_kind).into_owned(),
+                chunk_type: String::from_utf8_lossy(chunk_type).into_owned()
+            }
+        }
+    )(input)
+}
+
+// named!(parse_header<(u16, &str, String)>,
+//     do_parse!(
+//         version: match_version >>
+//         game_type: apply!(match_utf8, GAME_TYPE) >>
+//         timestamp: match_terminated_utf16 >>
+//         many_m_n!(7, 7, verify!(le_u32, |n: u32| n == 0)) >>
+//         (version, game_type, timestamp)
+//     )
+// );
+
+// named!(parse_chunky<bool>,
+//     do_parse!(
+//         apply!(match_utf8, CHUNKY_NAME) >>
+//         verify!(le_u32, |n| n == CHUNKY_TYPE) >>
+//         verify!(le_u32, |n| n == CHUNKY_VERSION) >>
+//         verify!(le_u32, |n| n == 0x1) >>
+//         verify!(le_u32, |n| n == 0x24) >>
+//         verify!(le_u32, |n| n == 0x1C) >>
+//         verify!(le_u32, |n| n == 0x1) >>
+//         (true)
+//     )
+// );
+
+// named!(test_eof<CompleteByteSlice, bool>,
+//     do_parse!(
+//         // many_till!(g_le_u16, eof!()) >>
+//         count!(g_le_u8, 2000000) >>
+//         (true)
+//     )
+// );
+
+// named!(test_eof_slice<bool>,
+//     do_parse!(
+//         count!(g_le_u8, 2000000) >>
+//         (true)
+//     )
+// );
+
+// // fn test_eof_long(input: CompleteByteSlice) -> IResult<CompleteByteSlice, CompleteByteSlice> {
+// //     many_till!(input, take!(1), eof!())
+// // }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_parse_header_ok() {
+//         let buff = read_into_buffer(Path::new("/Users/ryantaylor/Code/vault/replays/bench.rec"));
+//         let (_, (version, game_type, timestamp)) = parse_header(&buff).unwrap();
+//         assert_eq!(version, 20297);
+//         assert_eq!(game_type, "COH2_REC");
+//         assert_eq!(timestamp, "11/7/2015 1:16 AM");
+//     }
+
+//     #[test]
+//     fn test_parse_chunky_ok() {
+//         let buff = read_into_buffer(Path::new("/Users/ryantaylor/Code/vault/replays/bench.rec"));
+//         let (remaining, _) = parse_header(&buff).unwrap();
+//         let (_, result) = parse_chunky(remaining).unwrap();
+//         assert!(result);
+//     }
+
+//     #[test]
+//     fn test_parse_eof() {
+//         let buff = read_into_buffer(Path::new("/Users/ryantaylor/Code/vault/replays/bench.rec"));
+//         let (remaining, _) = test_eof(CompleteByteSlice(&buff)).unwrap();
+//     }
+
+//     #[bench]
+//     fn bench_parse_eof(b: &mut Bencher) {
+//         let buff = read_into_buffer(Path::new("/Users/ryantaylor/Code/vault/replays/bench.rec"));
+//         b.iter(|| {
+//             test_eof(CompleteByteSlice(&buff)).unwrap()
+//             // println!("{:?}", remaining);
+//             // println!("{:?}", val);
+//         });
+//     }
+
+//     #[bench]
+//     fn bench_parse_eof_slice(b: &mut Bencher) {
+//         let buff = read_into_buffer(Path::new("/Users/ryantaylor/Code/vault/replays/bench.rec"));
+//         b.iter(|| {
+//             test_eof_slice(&buff).unwrap()
+//             // println!("{:?}", remaining);
+//         });
+//     }
+
+//     fn read_into_buffer(path: &Path) -> Vec<u8> {
+//         let mut file = File::open(path).unwrap();
+//         let mut buff: Vec<u8> = Vec::new();
+//         file.read_to_end(&mut buff).unwrap();
+//         buff
+//     }
+// }
