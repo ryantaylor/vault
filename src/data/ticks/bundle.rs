925930778ad5efe29f539e75e97565e398d726ca
ryantaylor <2320507+ryantaylor@users.noreply.github.com>
2024-06-03 10:28:58 -0400

    Handle bundles with more than one command.

--- src/data/ticks/bundle.rs
+++ src/data/ticks/bundle.rs
@@ -2,23 +2,27 @@ use super::Command;
 use crate::data::{ParserResult, Span};
 use nom::bytes::complete::take;
 use nom::combinator::map;
-use nom::multi::length_value;
+use nom::multi::{length_value, many1};
 use nom::number::complete::le_u32;
 use nom::sequence::tuple;
 use nom_tracable::tracable_parser;
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Clone)]
 pub struct Bundle {
     pub index: u32,
-    pub command: Command,
+    pub commands: Vec<Command>,
 }
 
 impl Bundle {
     #[tracable_parser]
     pub fn parse_bundle(input: Span) -> ParserResult<Bundle> {
         map(
-            tuple((le_u32, take(4u32), length_value(le_u32, Command::parse))),
-            |(index, _, command)| Bundle { index, command },
+            tuple((
+                le_u32,
+                take(4u32),
+                length_value(le_u32, many1(Command::parse)),
+            )),
+            |(index, _, commands)| Bundle { index, commands },
         )(input)
     }
 }
